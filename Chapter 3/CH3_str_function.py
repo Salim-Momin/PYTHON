b4a4ea7edccbd6821d5dc16ba7b73a1d4faae646
Salim-Momin <salim.momin123231@gmail.com>
2025-07-19 08:51:58 +0530

    Chapter 3

---
+++ Chapter 3/CH3_str_function.py
@@ -0,0 +1,15 @@
+#use len function for check the lenght of string
+
+name = "salim"
+
+print(len(name))
+
+#the endswith functions use for check the string ends with correct character or not
+
+print(name.endswith("im"))
+
+#the similar function is startswith for check the string start with correct character or not
+
+print(name.startswith("s"))
+
+prin
